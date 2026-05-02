e381d4d5e072b642ee877e84998376113f03e927
Adwaith Pramod K <adwaithpramod786@gmail.com>
2026-05-02 17:57:59 +0530

    MadLibs Simple String Learning

---
+++ Mini Projects/MadLibs.py
@@ -0,0 +1,9 @@
+print("Simple Mad Libs Game")
+
+name = input("Enter a Name :").strip()
+place = input("Enter a place :").strip()
+adj = input("Enter an Adjective :").strip()
+ani = input("Enter an animal :").strip()
+verb = input("Enter a verb :").strip()
+
+print(f"One day, {name} went to the {place}. It was a {adj} day. Suddenly, a {ani} appeared and started {verb}. Everyone around was shocked and started laughing!")
